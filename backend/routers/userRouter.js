ade071c0f04871af7370a5d3a8e54d26eb91f795
koushikpranank <eslavathpranank@gmail.com>
2026-07-31 15:54:23 +0530

    Describe your changes here

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,10 +1,15 @@
-const express= require("express");
-const router=express.Router();
-const { register, login, getAllUsers, deleteUser } = require("../controller/UserController");
-
+const express = require("express");
+const router = express.Router();
+const {
+  register,
+  login,
+  getAllUsers,
+  deleteUser,
+} = require("../controller/UserController");
+const { isAdmin } = require("../middleware/userRoleAuth");
 router.post("/register", register);
 router.post("/login", login);
-router.get("/getallusers", getAllUsers);
-router.delete("/deleteuser/:id", deleteUser);
+router.get("/getallusers", isAdmin, getAllUsers);
+router.delete("/deleteuser/:id", isAdmin, deleteUser);
 
-module.exports=router;
+module.exports = router;
