14f0608e55d13d246a309938a25e137113e75c33
koushikpranank <eslavathpranank@gmail.com>
2026-07-31 11:18:20 +0530

    Add your descriptive commit message here

--- backend/controller/ProductsController.js
+++ backend/controller/ProductsController.js
@@ -56,7 +56,7 @@ const UpdateProduct = async (req, res) => {
       req.body,
       { new: true },
     );
-    res.status(200).json({iPad
+    res.status(200).json({
       message: "updated Successfully",
       updatedProductDetails: updatedProduct,
     });
@@ -122,20 +122,24 @@ const getProductsWithPagination = async (req, res) => {
 };
 
 const getProductsBasedOnPrice = async (req, res) => {
-    try {
-        const { minPrice, maxPrice } = req.query;
-        const filteredProducts = await Products.find({
-            cost: { $gte: minPrice, $lte: maxPrice }
-        });
-        if(filteredProducts.length == 0) {
-            return res.status(404).json({ message: `No products found in the specified price range ${minPrice} To ${maxPrice}` });
-        } else {
-            res.status(200).json({ filteredProducts });
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Failed to filter products based on price" });
+  try {
+    const { minPrice, maxPrice } = req.query;
+    const filteredProducts = await Products.find({
+      cost: { $gte: minPrice, $lte: maxPrice },
+    });
+    if (filteredProducts.length == 0) {
+      return res.status(404).json({
+        message: `No products found in the specified price range ${minPrice} To ${maxPrice}`,
+      });
+    } else {
+      res.status(200).json({ filteredProducts });
     }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Failed to filter products based on price" });
+  }
 };
 
 module.exports = {
@@ -147,5 +151,5 @@ module.exports = {
   FilterProductsOnPrice,
   FilterProductsOnRatings,
   getProductsWithPagination,
-  getProductsBasedOnPrice
+  getProductsBasedOnPrice,
 };
